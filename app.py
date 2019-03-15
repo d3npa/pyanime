b01e3ce4ba01fc26f0b72a8436a93165353c991c
d3npä <d3npa@protonmail.com>
2019-03-15 10:45:53 -0400

    リスナーIPを修正しました

--- app.py
+++ app.py
@@ -85,4 +85,4 @@ def episode(site, url, quality="720"): # エピソード一覧を取得する
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
-    app.run(host='0.0.0.0', port=port)
+    app.run(host='127.0.0.1', port=port)
